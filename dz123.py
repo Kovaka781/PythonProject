c3dedfc240243db46e7fe49371c3e54a5be982c6
kovaka <dk7537280@gmail.com>
2025-05-25 13:38:48 +0300

    DZ 65`

---
+++ dz123.py
@@ -0,0 +1,19 @@
+import logging
+
+#
+logging.basicConfig(
+    filename='error_log.txt',
+    level=logging.ERROR,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
+
+def divide_numbers(a, b):
+    try:
+        result = a / b
+        print(f"Результат: {result}")
+    except Exception as e:
+        logging.error("Помилка при діленні чисел", exc_info=True)
+        print("Виникла помилка. Деталі записані у файл 'error_log.txt'.")
+
+# Тестовий виклик
+divide_numbers(10, 0)
